c91f12de916914676cf5641dd6ee244cfc10b31d
Carla Stickler <carlastickler@gmail.com>
2019-01-30 00:20:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const main = document.getElementById('main')
 main.addEventListener('click', function(event) {
   alert('I was clicked!')
 })
+
+const input = document.querySelector('input')
+input.addEventListener('keydown', e => console.log(e.which))
