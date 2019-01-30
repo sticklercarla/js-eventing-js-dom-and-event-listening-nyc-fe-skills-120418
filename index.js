2a3d11fc1f64362c530e2ac12b8af0df9dffff22
Carla Stickler <carlastickler@gmail.com>
2019-01-30 00:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,32 +14,32 @@
 //     return e.preventDefault()
 //   }
 // })
-let divs = document.querySelectorAll('div')
+// let divs = document.querySelectorAll('div')
  
-function bubble(e) {
-  // remember all of those fancy DOM node properties?
-  // we're making use of them to get the number
-  // in each div here!
+// function bubble(e) {
+//   // remember all of those fancy DOM node properties?
+//   // we're making use of them to get the number
+//   // in each div here!
  
-  // if `this` is a bit confusing, don't worry —
-  // for now, know that it refers to the div that
-  // is triggering the current event handler.
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
-}
+//   // if `this` is a bit confusing, don't worry —
+//   // for now, know that it refers to the div that
+//   // is triggering the current event handler.
+//   console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+// }
  
-for (const aDiv of divs) {
-  aDiv.addEventListener('click', bubble);
-}
+// for (const aDiv of divs) {
+//   aDiv.addEventListener('click', bubble);
+// }
 
-divs = document.querySelectorAll('div')
+// divs = document.querySelectorAll('div')
  
-function capture(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' captured')
-}
+// function capture(e) {
+//   console.log(this.firstChild.nodeValue.trim() + ' captured')
+// }
  
-for (const aDiv of divs) {
-  // set the third argument to `true`!
-  aDiv.addEventListener('click', capture, true)
-}
+// for (const aDiv of divs) {
+//   // set the third argument to `true`!
+//   aDiv.addEventListener('click', capture, true)
+// }
 
 
