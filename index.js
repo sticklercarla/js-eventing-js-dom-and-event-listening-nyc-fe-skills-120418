d62d761c9427c28ad4c71d500cb76aec941b475d
Carla Stickler <carlastickler@gmail.com>
2019-01-30 00:25:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,19 @@
 //     return e.preventDefault()
 //   }
 // })
+let divs = document.querySelectorAll('div')
+ 
+function bubble(e) {
+  // remember all of those fancy DOM node properties?
+  // we're making use of them to get the number
+  // in each div here!
+ 
+  // if `this` is a bit confusing, don't worry —
+  // for now, know that it refers to the div that
+  // is triggering the current event handler.
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+ 
+for (const aDiv of divs) {
+  aDiv.addEventListener('click', bubble);
+}
